66c946935862f71d960cad3958a9f238f63978e7
Michelle Nguyen <michellenguyen@pixielabs.ai>
2022-01-31 14:13:53 -0800

    Initial version of addon
    Signed-off-by: Michelle Nguyen <michellenguyen@pixielabs.ai>

---
+++ README.md
@@ -0,0 +1,79 @@
+# Pixie AddOn for AWS SSP CDK Platform
+
+This repository contains the source code for the Pixie AddOn for AWS SSP CDK. `ssp-amazon-eks` is a [CDK](https://aws.amazon.com/cdk/) construct that makes it easy for customers to build and deploy a Shared Services Platform (SSP) on top of [Amazon EKS](https://aws.amazon.com/eks/).
+
+## Installation
+
+Using [npm](https://npmjs.org):
+
+```bash
+$ npm install @pixie-labs/pixie-ssp-addon
+```
+
+## Usage
+
+```
+import { App } from '@aws-cdk/core';
+import * as ssp from '@aws-quickstart/ssp-amazon-eks';
+import { PixieAddOn } from '@pixie-labs/pixie-ssp-addon';
+
+const app = new App();
+
+ssp.EksBlueprint.builder()
+    .addOns(new PixieAddOn({
+        deployKey: "pixie-deploy-key", // Create and copy from Pixie Admin UI
+    }))
+    .region(process.env.AWS_REGION)
+    .account(process.env.AWS_ACCOUNT)
+    .build(app, 'my-test-cluster');
+```
+
+## `PixieAddOn` Options (props)
+
+#### `deployKey: string` (required)
+
+Pixie deployment key (plain text).  Log into the Admin UI in Pixie to generate a deployment key. This attaches your Pixie deployment to your org.
+
+#### `namespace?: string` (optional)
+
+Namespace to deploy Pixie to. Default: `pl`
+
+#### `cloudAddr?: string` (optional)
+
+The address of Pixie Cloud. This should only be modified if you have deployed your own self-hosted Pixie Cloud. By default, it will be set to [Community Cloud for Pixie](https://work.withpixie.dev).
+
+#### `devCloudNamespace?: string` (optional)
+
+If running in a self-hosted cloud with no DNS configured, the namespace in which the self-hosted cloud is running. 
+
+#### `clusterName?: string` (optional)
+
+The name of cluster. If none is specified, a random name will be generated.
+
+#### `useEtcdOperator?: boolean` (optional)
+
+Whether the metadata store should use etcd to store metadata, or use a persistent volume store. If not specified, the operator will deploy based on the cluster's storageClass configuration.
+
+#### `pemMemoryLimit?: string` (optional)
+
+The memory limit applied to the PEMs (data collectors). Set to 2Gi by default.
+
+#### `dataAccess?: "Full"|"Restricted"|"PIIRestricted"` (optional)
+
+DataAccess defines the level of data that may be accesssed when executing a script on the cluster. If none specified, assumes full data access.
+
+#### `patches?: [key: string]: string` (optional)
+
+Custom K8s patches which should be applied to the Pixie YAMLs. The key should be the name of the K8s resource, and the value is the patch that should be applied.
+
+#### `version?: string` (optional)
+
+Helm chart version.
+
+#### `repository?: string`, `release?: string`, `chart?: string` (optional)
+
+Additional options for customers who may need to supply their own private Helm repository.
+
+## License
+pixie-ssp-addon is licensed under the [Apache 2.0](http://apache.org/licenses/LICENSE-2.0.txt) License.
+
---
+++ src/index.ts
@@ -0,0 +1,124 @@
+import { ClusterAddOn, ClusterInfo } from '@aws-quickstart/ssp-amazon-eks';
+import { Construct } from '@aws-cdk/core';
+
+export type DataAccessType = 'Full' | 'Restricted' | 'PIIRestricted';
+
+export interface PixieAddOnProps {
+    /**
+     * Helm chart repository.
+     * Defaults to the official repo URL.
+     */
+    repository?: string;
+
+    /**
+     * Release name.
+     * Defaults to 'pixie'.
+     */
+    release?: string;
+
+    /**
+     * Chart name.
+     * Defaults to 'pixie-operator-chart'.
+     */
+    chart?: string;
+
+    /**
+     * Helm chart version
+     */
+    version?: string;
+
+    /**
+     * Namespace for the add-on.
+     */
+    namespace?: string;
+
+    /** 
+     * Address for the Pixie Cloud instance to deploy to. Points to
+     * Community Cloud for Pixie by default.
+     */ 
+    cloudAddr?: string;
+
+    /**
+     * Deploy key from Pixie Cloud. Used to link the Pixie deployment
+     * to an org.
+     */ 
+    deployKey?: string;
+
+    /**
+     * Kubernetes cluster name.
+     */
+    clusterName?: string;
+
+    /**
+     * If running in a self-hosted cloud with no DNS configured, the namespace
+     * in which the self-hosted cloud is running. 
+     */ 
+    devCloudNamespace?: string;
+
+    /** 
+     * Whether the metadata store should use etcd to store metadata, or use a 
+     * persistent volume store.
+     */
+    useEtcdOperator?: boolean;
+
+    /**
+     * Custom K8s patches which should be applied to the Pixie YAMLs. The key should be
+     * the name of the K8s resource, and the value is the patch that should be applied.
+     */ 
+    patches?: {
+	[key: string]: string;
+    }
+
+    /**
+     * The memory limit applied to the PEMs (data collectors). Set to 2Gi by default.
+     */
+    pemMemoryLimit?: string;
+
+    /**
+     * DataAccess defines the level of data that may be accesssed when executing a script 
+     * on the cluster. If none specified, assumes full data access.
+     */
+    dataAccess?: DataAccessType;
+
+}
+
+const defaultProps: PixieAddOnProps = {
+    repository: "https://pixie-operator-charts.storage.googleapis.com",
+    release: "pixie",
+    chart: "pixie-operator-chart",
+    version: "0.0.18",
+    namespace: "pl",
+    cloudAddr: "withpixie.ai:443",
+    useEtcdOperator: false,
+    pemMemoryLimit: "2Gi",
+    dataAccess: "Full",
+};
+
+export class PixieAddOn implements ClusterAddOn {
+    readonly options: PixieAddOnProps;
+
+    constructor(props?: PixieAddOnProps) {
+        this.options = {...defaultProps, ...props};
+    }
+
+    deploy(clusterInfo: ClusterInfo): Promise<Construct> {
+    	const props = this.options;
+
+	const pixieHelmChart = clusterInfo.cluster.addHelmChart("pixie", {
+            chart: props.chart,
+            release: props.release,
+            repository: props.repository,
+            namespace: props.namespace,
+            version: props.version,
+            values: {
+		deployKey: props.deployKey,
+		cloudAddr: props.cloudAddr,
+		useEtcdOperator: props.useEtcdOperator,
+		clusterName: props.clusterName,
+		devCloudNamespace: props.devCloudNamespace,
+		patches: props.patches,	
+	    }
+        });
+        return Promise.resolve(pixieHelmChart);
+    }
+}
